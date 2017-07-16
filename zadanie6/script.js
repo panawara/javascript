0757e9c752a279517cc6def95458cf6ce4e911c1
panawara <pa.nawara@gmail.com>
2017-07-16 14:08:35 +0200

    corrected exercise 6, if, else, if, else

--- zadanie6/script.js
+++ zadanie6/script.js
@@ -3,15 +3,12 @@ var b = 2;
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log(value);
 
-
-if (value>0) {
-	console.log("dodatnia")
-} else if (value<0) {
-	console.log("ujemna")
-}
-
-if (value==0) {
-	console.log("zero")
+if (value > 0) {
+	console.log("dodatnia");
 } else {
-	console.log("nie-zero")
+	console.log("ujemna");
+} if (value == 0) {
+	console.log("zero");
+} else {
+	console.log("nie-zero");
 }
